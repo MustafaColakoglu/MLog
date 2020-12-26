c0d42fc0198b3bdc46c962139b5e33a238b3ea20
MustafaColakoglu <mustafacolakoglu7@gmail.com>
2020-12-26 16:00:55 +0300

    init commit

---
+++ MLog/Concrete/MLog.cs
@@ -0,0 +1,48 @@
+﻿using MLog.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace MLog.Concrete
+{
+    public class MLog : IMLog
+    {
+        private string fileName;
+        private string fileFullPath;
+        private string currentPath;
+        private string dir;
+        public MLog()
+        {
+            fileName =$"{DateTime.Now.ToString("dd.MM.yyyy")}.txt";
+            currentPath = AppDomain.CurrentDomain.BaseDirectory;
+
+            dir = $@"{currentPath}Mlogs";
+            fileFullPath = $@"{dir}\{fileName}";
+
+
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+
+            if (!File.Exists(fileFullPath))
+            {
+                File.Create(fileFullPath);
+            }
+        }
+        public void Info(string message)
+        {
+            StreamWriter sw = new StreamWriter(fileFullPath);
+            string methodName=MethodBase.GetCurrentMethod().Name;
+
+            string log = $"{DateTime.Now:dd.MM.yyyy HH:mm} | {methodName}";
+
+            sw.WriteLine(log);
+                     
+            sw.Close();
+        }
+    }
+}
---
+++ MLogTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using MLog.Abstract;
+using MLog.Concrete;
+using System;
+
+namespace MLogTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MLog.Concrete.MLog mLog = new MLog.Concrete.MLog();
+
+            mLog.Info("test");
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
